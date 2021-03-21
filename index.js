943f0b25fd362d9c3af3a19e3c8d4a6dba51ced7
Kutay Furtun <kfurtun@gmail.com>
2021-03-20 22:21:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function rollDice() {
+  let dice1Value = Math.floor(Math.random() * 6) + 1;
+  let dice2Value = Math.floor(Math.random() * 6) + 1;
+
+  document.getElementById("firstDice").className = "dice dice" + dice1Value;
+  document.getElementById("secondDice").className = "dice dice" + dice2Value;
+}
